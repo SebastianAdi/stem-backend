f4b225d82af0711ef0ba8f7ae0aa11d0e0cb6c62
Aditya Prakash <aditya.prakash.in@freecharge.com>
2023-05-30 11:59:04 +0530

    adding sql files

---
+++ src/main/resources/department.sql
@@ -0,0 +1,20 @@
+/*
+-- Query: select * from stemmedics.department
+LIMIT 0, 1000
+
+-- Date: 2023-05-30 11:34
+*/
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (1,'Anesthetics');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (2,'Cardiology');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (3,'Ear, Nose and Throat(ENT)');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (4,'Gastroenterology');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (5,'General Surgery');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (6,'Gynecology');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (7,'Hematology');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (8,'Neurology');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (9,'Nutrition and dietetics');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (10,'Orthopedics');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (11,'Oncology');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (12,'Physiotherapy');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (13,'Renal Unit');
+INSERT INTO `` (`dept_id`,`dept_name`) VALUES (14,'Urology');
---
+++ src/main/resources/doctor.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: select * from stemmedics.doctor
+LIMIT 0, 1000
+
+-- Date: 2023-05-30 11:32
+*/
+INSERT INTO `` (`doctor_id`,`city`,`email`,`experience`,`hospital`,`name`,`phone_no`,`department`,`password`) VALUES (1,'Bijnor','dr.rajesh.sharma@gmail.com',5,'Aayushmaan Hospital','Dr. Rajesh Sharma','9876543210','Cardiology','wordpass');
+INSERT INTO `` (`doctor_id`,`city`,`email`,`experience`,`hospital`,`name`,`phone_no`,`department`,`password`) VALUES (2,'Varanasi','pooja.verma@gmail.com',10,'Alliance Hospital','Dr. Pooja Verma','8765432109','Neurology','secret');
+INSERT INTO `` (`doctor_id`,`city`,`email`,`experience`,`hospital`,`name`,`phone_no`,`department`,`password`) VALUES (3,'Lucknow','sanjay.patel@gmail.com',6,'Arushi Medical Centre','Dr. Sanjay Patel','7654321098','Physiotherapy','sanjay123');
+INSERT INTO `` (`doctor_id`,`city`,`email`,`experience`,`hospital`,`name`,`phone_no`,`department`,`password`) VALUES (4,'Lucknow','reena.singhania@gmail.com',7,'Arushi Medical Centre','Dr. Reena Singhania','6543210987','Physiotherapy','#reena12');
+INSERT INTO `` (`doctor_id`,`city`,`email`,`experience`,`hospital`,`name`,`phone_no`,`department`,`password`) VALUES (5,'Lucknow','arjun.mehta@gmail.com',10,'Divine Heart Hospital & Resesarch Centre','Dr. Arjun Mehta','5432109876','Physiotherapy','Arjun12345');
+INSERT INTO `` (`doctor_id`,`city`,`email`,`experience`,`hospital`,`name`,`phone_no`,`department`,`password`) VALUES (6,'Lucknow','nisha.reddy@gmail.com',12,'Divine Heart Hospital & Resesarch Centre','Dr. Nisha Reddy','4321098765','Physiotherapy','#qwerty');
+INSERT INTO `` (`doctor_id`,`city`,`email`,`experience`,`hospital`,`name`,`phone_no`,`department`,`password`) VALUES (7,'Lucknow','ajay.kapoor@gmail.com',6,'Divine Heart Hospital & Resesarch Centre','Dr. Ajay Kapoor','3210987654','Urology','#qwerty123');
+INSERT INTO `` (`doctor_id`,`city`,`email`,`experience`,`hospital`,`name`,`phone_no`,`department`,`password`) VALUES (8,'Lucknow','abhishek.singh@example.com',10,'Avadh Hospital & Heart Centre','Dr. Abhishek Singh','9876543211','Cardiology','mysecurepassword1');
+INSERT INTO `` (`doctor_id`,`city`,`email`,`experience`,`hospital`,`name`,`phone_no`,`department`,`password`) VALUES (9,'Lucknow','jonita.verma@example.com',8,'City Hospital& Trauma Centre','Dr. Jonita Verma','9876543212','Orthopedics','mypassword3');
+INSERT INTO `` (`doctor_id`,`city`,`email`,`experience`,`hospital`,`name`,`phone_no`,`department`,`password`) VALUES (10,'Agra','anurag.verma@example.com',5,'Asopa Hospital & Research Centre','Dr. Anurag Verma','9876543215','Ear, Nose and Throat(ENT)','drowssap');
